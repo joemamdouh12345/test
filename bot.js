f9f5fc017fb1c81adddf0f9491cfeacddb26ba6d
joemamdouh12345 <48876276+joemamdouh12345@users.noreply.github.com>
2019-04-22 09:04:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -42,7 +42,7 @@ j = 1;
             j = -1;    
       }    
        i = i+j;    
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);    
+        client.user.setGame(setGame[i],`https://www.twitch.tv/Allwai`);    
 }, ms);    
     
 });
